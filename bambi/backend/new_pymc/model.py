122a4e1f5a28db9180f192924a78d21500c84954
Tomas Capretto <tomicapretto@gmail.com>
2026-02-09 13:03:45 -0300

    Progress

--- bambi/backend/new_pymc/model.py
+++ bambi/backend/new_pymc/model.py
@@ -14,18 +14,31 @@ from bambi.backend.new_pymc.coords import (
 
 class Model:
     def __init__(self, model):
-        self.pymc_model = None
-        self.bambi_model = model
-        self._response_coords = coords_from_response(model.response_term)
+        """_summary_
+
+        Parameters
+        ----------
+        model : bmb.Model
+            The bambi model specification.
+        """
+        self.spec = model
+        self.model = None
+
+        self.response_coords = coords_from_response(self.spec.response_term)
         self.conditional_parameters = {
-            key: ConditionalParameter(value) for key, value in model.conditional_parameters.items()
+            key: ConditionalParameter(value)
+            for key, value in self.spec.conditional_parameters.items()
         }
         self.marginal_parameters = {
-            key: MarginalParameter(value) for key, value in model.marginal_parameters.items()
+            key: MarginalParameter(value) for key, value in self.spec.marginal_parameters.items()
         }
 
     def get_coords(self):
-        coords = self._response_coords.copy()
+        coords = self.response_coords.copy()
+
+        for parameter in self.conditional_parameters.values():
+            for term in parameter.terms.values():
+                coords.update(term.coords)
 
         for parameter in self.bambi_model.conditional_parameters.values():
             for term in parameter.common_terms.values():
@@ -66,93 +79,3 @@ class Model:
         # 4. Build expressions, grabbing them from the PyMC Model instance.
 
         # 5. Build response, again grabbing stuff from the PyMC model.
-
-
-class CommonTerm:
-    def __init__(self, term):
-        self.term = term
-        self.coords = coords_from_common(self.term)
-        self.data_name = f"{self.term.name}_data"
-        self.data_dims = None
-        self.param_name = None
-
-    def register_data(self, model):
-        if self.data_name in model:
-            return None
-
-        output_ndim = model.__bambi_attrs__["output_ndim"]
-        self.data_dims = ["__obs__", *self.coords, *model.__bambi_attrs__["output_coords"]]
-
-        if output_ndim == 1:
-            data_shape = (self.term.data.shape[0], -1)
-        elif output_ndim == 2:
-            data_shape = (self.term.data.shape[0], -1, self.term.data.shape[-1])
-        else:
-            raise ValueError("panic")
-
-        data = np.reshape(self.term.data, data_shape)
-        pm.Data(self.data_name, data, dims=self.data_dims, model=model)
-
-    def register_parameter(self, model):
-        # NOTE:
-        #   When repsonse_ndim == 2 we need to use `pt.atleast_2d`
-        #   Otherwise, we use `pt.atleast_1d`.
-        # NOTE:
-        #   Do we check if it's in the model before adding it?
-        # NOTE:
-        #   When len(term.coords) > 1, we need to reshape.
-        #   When output_ndim == 1: x.reshape(-1)
-        #   When output_ndim == 2: x.reshape(-1, x.shape[-1])
-        coords = self.coords | model.__bambi_attrs__["output_coords"]
-        dims = tuple(coords)
-        shape = tuple(len(coord) for coord in coords.values())
-
-        kwargs = {
-            name: np.broadcast_to(value, shape) for name, value in self.term.prior.args.items()
-        }
-
-        dist = get_distribution_from_prior(self.term.prior)
-        dist(self.param_name, **kwargs, dims=dims, model=model)
-
-
-class MarginalParameter:
-    """Global, free parameter shared across observations."""
-
-
-class ConditionalParameter:
-    """Deterministic parameter computed as a function of data and other parameters."""
-
-    def __init__(self, parameter):
-        self.bambi_parameter = parameter
-
-    def build(self, model):
-        self.build_common(model)
-
-    def build_common(self, model):
-        terms = []
-
-    def build_group_specific(self):
-        pass
-
-
-MAPPING = {"Cumulative": pm.Categorical, "StoppingRatio": pm.Categorical}
-
-
-def get_distribution(dist):
-    """Return a PyMC distribution."""
-    if isinstance(dist, str):
-        if dist in MAPPING:
-            dist = MAPPING[dist]
-        elif hasattr(pm, dist):
-            dist = getattr(pm, dist)
-        else:
-            raise ValueError(f"The Distribution '{dist}' was not found in PyMC")
-    return dist
-
-
-def get_distribution_from_prior(prior):
-    if prior.dist is not None:
-        distribution = prior.dist
-    else:
-        distribution = get_distribution(prior.name)
-    return distribution
---
+++ bambi/backend/new_pymc/terms/__init__.py
@@ -0,0 +1,7 @@
+from .common import build_common_term
+from .intercept import build_intercept_term
+
+__all__ = [
+    "build_common_term",
+    "build_intercept_term",
+]
