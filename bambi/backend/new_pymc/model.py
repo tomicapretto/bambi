5f341caa19cbf227be7dd8df4521c11d400df572
Tomas Capretto <tomicapretto@gmail.com>
2026-02-17 13:40:34 -0300

    Progress

--- bambi/backend/new_pymc/model.py
+++ bambi/backend/new_pymc/model.py
@@ -2,6 +2,8 @@ import numpy as np
 import pymc as pm
 import pytensor.tensor as pt
 
+from bambi.backend.new_pymc import coords
+from bambi.backend.new_pymc.parameters import ConditionalParameter, MarginalParameter
 from bambi.backend.new_pymc.coords import (
     coords_from_common,
     coords_from_group_specific,
@@ -21,10 +23,8 @@ class Model:
         model : bmb.Model
             The bambi model specification.
         """
-        self.spec = model
         self.model = None
-
-        self.response_coords = coords_from_response(self.spec.response_term)
+        self.spec = model
         self.conditional_parameters = {
             key: ConditionalParameter(value)
             for key, value in self.spec.conditional_parameters.items()
@@ -32,22 +32,6 @@ class Model:
         self.marginal_parameters = {
             key: MarginalParameter(value) for key, value in self.spec.marginal_parameters.items()
         }
-
-    def get_coords(self):
-        coords = self.response_coords.copy()
-
-        for parameter in self.conditional_parameters.values():
-            for term in parameter.terms.values():
-                coords.update(term.coords)
-
-        for parameter in self.bambi_model.conditional_parameters.values():
-            for term in parameter.common_terms.values():
-                coords.update(coords_from_common(term))
-
-            for term in parameter.group_specific_terms.values():
-                coords.update(coords_from_group_specific(term))
-
-        return coords
 
     def build(self):
         ## Global process:
@@ -61,21 +45,42 @@ class Model:
         # 3. Create deterministics (grab stuff from PyMC model)
 
         # 1. Build dims, coordinates, and term
-        # for parameter in self.bambi_model.marginal_parameters:
-        #     ...
-        coords = self.get_coords()
-        model = pm.Model(coords=coords)
+        output_coords = coords_from_response(self.spec.response_term)
+        output_coords.update(
+            {
+                "__obs__": np.arange(self.spec.response_term.data.shape[0]),
+            }
+        )
+        model = pm.Model(coords=output_coords)
+        model.__bambi_attrs__ = {
+            "output_ndim": self.spec.family.output_ndim,
+            "output_coords": output_coords,
+        }
 
-        with pm.Model(coords=coords) as model:
-            # 2. Build data containers -> Has
-            for parameter in self.bambi_model.conditional_parameters.values():
-                # for term in parameter
-                pass
-
-        self.pymc_model = model
+        for parameter in self.conditional_parameters.values():
+            parameter.build(model)
 
         # 3. Build random variables
 
         # 4. Build expressions, grabbing them from the PyMC Model instance.
 
         # 5. Build response, again grabbing stuff from the PyMC model.
+
+        self.response_coords = coords_from_response(self.spec.response_term)
+
+
+def get_coords(self):
+    coords = self.response_coords.copy()
+
+    for parameter in self.conditional_parameters.values():
+        for term in parameter.terms.values():
+            coords.update(term.coords)
+
+    for parameter in self.bambi_model.conditional_parameters.values():
+        for term in parameter.common_terms.values():
+            coords.update(coords_from_common(term))
+
+        for term in parameter.group_specific_terms.values():
+            coords.update(coords_from_group_specific(term))
+
+    return coords
--- bambi/backend/new_pymc/parameters/conditional.py
+++ bambi/backend/new_pymc/parameters/conditional.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pymc as pm
 import pytensor.tensor as pt
+import pytensor.sparse as ps
+import scipy as sp
 
 from bambi.backend.new_pymc.terms import build_common_term, build_intercept_term
+from bambi.config import config as bmb_config
 
 
 class ConditionalParameter:
@@ -31,9 +34,6 @@ class ConditionalParameter:
         return ensure_ndim(build_intercept_term(self.spec.intercept_term, model))
 
     def build_common(self, model):
-        if not self.spec.common_terms:
-            return 0
-
         data_list = []
         param_list = []
 
@@ -49,15 +49,143 @@ class ConditionalParameter:
             param_list.append(ensure_ndim(param))
 
         params = pt.concatenate(param_list, axis=0)  # (p, ) or (p, K)
-        data = np.column_stack(data_list)  # (n, p)
+        data = pt.concatenate(data_list, axis=1)  # (n, p)
 
-        model.add_coord(f"{self.spec.name}_data_dim_1", np.arange(data.shape[1]))
-        data = pm.Data(
-            f"{self.spec.name}_data", data, dims=("__obs__", f"{self.spec.name}_data_dim_1")
-        )
-
+        # TODO: Use deterministic if we center covariates.
         # 'pt.dot(data, params)' is of shape (n, ) or (n, K)
         return pt.dot(data, params)
 
     def build_group_specific(self):
-        pass
+        as_multivariate = False
+        coefs = []
+        columns = []
+        predictors = []
+        group_indexes = []
+
+        # columns.append(term.data)
+        # predictors.append(term.predictor)
+        # group_indexes.append(term.group_index)
+
+        if bmb_config["SPARSE_DOT"]:
+            coefs_reshaped = []
+            for coef in coefs:
+                if as_multivariate and coef.ndim == 3:
+                    # (f_j, e_j, k) -> (f_j * e_j, k)
+                    coef_reshaped = coef.reshape(-1, coef.shape[-1])
+                elif not as_multivariate and coef.ndim == 2:
+                    # (f_j, e_j) -> (f_j * e_j,)
+                    coef_reshaped = coef.flatten()
+                else:
+                    coef_reshaped = coef
+
+                coefs_reshaped.append(coef_reshaped)
+
+            # Design matrix Z: shape (n, q)
+            data = sp.sparse.hstack(columns, format="csr")
+
+            # Coefficients: shape (q, ) or (q, k)
+            coefs = pt.concatenate(coefs_reshaped, axis=0)
+
+            if not as_multivariate:
+                coefs = coefs[:, np.newaxis]  # PyTensor expects 2D
+
+            contribution = ps.structured_dot(data, coefs).squeeze()  # (n, ) or (n, K)
+        else:
+            for coef, predictor, group_index in zip(coefs, predictors, group_indexes):
+                # The following code is short, but not simple.
+                #
+                # With multivariate models, we have:
+                # When predictor.ndim > 1
+                #     (n, e_j, k) * (n, e_j, 1) -> (n, e_j, k)
+                #     (n, e_j, k).sum(1) -> (n, k)
+                # Else
+                #     (n, k) * (n, 1) -> (n, k)
+                #
+                # And with univariate models, we have:
+                # When predictor.ndim > 1
+                #     (n, e_j) * (n, e_j) -> (n, e_j)
+                #     (n, e_j).sum(1) -> (n, )
+                # Else
+                #     (n, ) * (1, ) -> (n, )
+                coef = coef[group_index]
+                predictor_ndim = predictor.ndim
+
+                if as_multivariate:
+                    predictor = predictor[:, np.newaxis]
+
+                term_contribution = coef * predictor
+
+                if predictor_ndim > 1:
+                    term_contribution = term_contribution.sum(axis=1)
+
+                contribution += term_contribution
+
+        for term in self.spec.group_specific_terms.values():
+            group_specific_term = GroupSpecificTerm(term, bmb_model.noncentered)
+            # Add coords
+            for name, values in group_specific_term.coords.items():
+                if name not in pymc_backend.model.coords:
+                    pymc_backend.model.add_coords({name: values})
+
+            coefs.append(group_specific_term.build(bmb_model))
+            columns.append(term.data)
+            predictors.append(term.predictor)
+            group_indexes.append(term.group_index)
+
+        if bmb_config["SPARSE_DOT"]:
+            coefs_reshaped = []
+            for coef in coefs:
+                if as_multivariate and coef.ndim == 3:
+                    # (f_j, e_j, k) -> (f_j * e_j, k)
+                    coef_reshaped = coef.reshape(-1, coef.shape[-1])
+                elif not as_multivariate and coef.ndim == 2:
+                    # (f_j, e_j) -> (f_j * e_j,)
+                    coef_reshaped = coef.flatten()
+                else:
+                    coef_reshaped = coef
+
+                coefs_reshaped.append(coef_reshaped)
+
+            # Design matrix Z: shape (n, q)
+            data = sp.sparse.hstack(columns, format="csr")
+
+            # Coefficients: shape (q, ) or (q, k)
+            coefs = pt.concatenate(coefs_reshaped, axis=0)
+
+            if not as_multivariate:
+                coefs = coefs[:, np.newaxis]  # PyTensor expects 2D
+
+            contribution = ps.structured_dot(data, coefs).squeeze()  # (n, ) or (n, k)
+        else:
+            contribution = 0
+            for coef, predictor, group_index in zip(coefs, predictors, group_indexes):
+                # The following code is short, but not simple.
+                #
+                # With multivariate models, we have:
+                # When predictor.ndim > 1
+                #     (n, e_j, k) * (n, e_j, 1) -> (n, e_j, k)
+                #     (n, e_j, k).sum(1) -> (n, k)
+                # Else
+                #     (n, k) * (n, 1) -> (n, k)
+                #
+                # And with univariate models, we have:
+                # When predictor.ndim > 1
+                #     (n, e_j) * (n, e_j) -> (n, e_j)
+                #     (n, e_j).sum(1) -> (n, )
+                # Else
+                #     (n, ) * (1, ) -> (n, )
+                coef = coef[group_index]
+                predictor_ndim = predictor.ndim
+
+                if as_multivariate:
+                    predictor = predictor[:, np.newaxis]
+
+                term_contribution = coef * predictor
+
+                if predictor_ndim > 1:
+                    term_contribution = term_contribution.sum(axis=1)
+
+                contribution += term_contribution
+
+        # 'contribution' is of shape (n, ) or (n, k)
+        self.output += contribution
