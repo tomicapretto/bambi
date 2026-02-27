d80888ddb289abbf9f450574c2d66c333ae4582c
Tomas Capretto <tomicapretto@gmail.com>
2026-02-27 10:31:25 -0300

    Some progress

--- bambi/backend/new_pymc/terms/group_specific.py
+++ bambi/backend/new_pymc/terms/group_specific.py
@@ -6,17 +6,34 @@ from bambi.backend.new_pymc.coords import coords_from_group_specific
 from bambi.backend.new_pymc.utils import get_distribution_from_prior
 
 
-def build_group_specific_term(term, model):
+# Data will be either a matrix or a vector.
+# If it is a matrix, it can be
+# - (__obs__, *dims_factor)
+# - (__obs__, *(expr_factor + dims_factor))
+#
+# I receive the data in (__obs__, *(expr_factor + dims_factor)) shape
+# I need to reshape it to (__obs__, *expr_factor, *dims_factor) for registration
+# And then I need to reshape back to (n, q) for dot product
+
+
+def build_group_specific_term_for_dot(term, model):
     # FIXME WIP
     coords_expr, coords_factor = coords_from_group_specific(term)
     dims_expr = tuple(coords_expr) or None
     dims_factor = tuple(coords_factor) or None
 
     # Register data
+    # FIXME: handle aliases
+    expr_data_name = f"{term.expr.name}_data"
+    factor_data_name = f"{term.factor.name}_data"
+
+    # if data_name not in model or param_name not in model:
+    #     model.add_coords(coords)
+
     data = ...
 
     # Register parameter
-    parameter = _build_distribution(
+    param = build_distribution(
         prior=term.prior,
         label=term.name,
         dims_expr=dims_expr,
@@ -25,10 +42,15 @@ def build_group_specific_term(term, model):
         model=model,
     )
 
+
+def build_group_specific_term(term, model):
+    # Register data
+    data = ...
+
     return None  # FIXME
 
 
-def _build_distribution(prior, label, dims_expr, dims_factor, noncentered, model):
+def build_distribution(prior, label, dims_expr, dims_factor, noncentered, model):
     # NOTE: Do we really need this recursive function? Do we really allow for > 2 level hierarchies?
     kwargs = {}
     for key, value in prior.args.items():
@@ -36,7 +58,7 @@ def _build_distribution(prior, label, dims_expr, dims_factor, noncentered, model
             # hyperparam_key = term.hyperprior_alias.get(key, key) # FIXME?
             hyperparam_key = key
             hyperparam_label = f"{label}_{hyperparam_key}"
-            kwargs[key] = _build_distribution(
+            kwargs[key] = build_distribution(
                 prior=value,
                 label=hyperparam_label,
                 dims_expr=dims_expr,
