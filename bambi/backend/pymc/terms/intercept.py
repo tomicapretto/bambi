a94fd4060d989ab3aa9d0b6b77ecdf28e3ad9244
Tomas Capretto <tomicapretto@gmail.com>
2026-07-05 15:59:07 +0300

    Make more progress

--- bambi/backend/pymc/terms/intercept.py
+++ bambi/backend/pymc/terms/intercept.py
@@ -3,18 +3,31 @@ import pymc as pm
 import pytensor.tensor as pt
 
 from bambi.backend.pymc.utils import get_distribution_from_prior
+from bambi.types import CoefSpec, Constraint
 
 
-def build_intercept_term(term, data_mean, common_params, model):
-    coords = model.__bambi_attrs__["response_coords_reduced"]
+def build_intercept_term(
+    term,
+    data_mean: np.ndarray | None,
+    common_params: pt.Variable | None,
+    coef_spec: CoefSpec,
+    model: pm.Model,
+) -> pt.Variable:
+    # NOTE (idea): Transform the intercept prior such that users can pass it cleanly.
+    coords = {}
+    if coef_spec.ndim > 0:
+        if coef_spec.constraint == Constraint.REFERENCE:
+            coords = model.__bambi_attrs__["response_coords_reduced"]
+        else:
+            coords = model.__bambi_attrs__["response_coords"]
+
     dims = tuple(coords)
     param_shape = tuple(len(coord) for coord in coords.values())
     kwargs = {name: np.broadcast_to(value, param_shape) for name, value in term.prior.args.items()}
     dist = get_distribution_from_prior(term.prior)
 
     with model:
-        if data_mean is not None:
-            # TODO: Automatic transformation of intercept prior such that users can pass it cleanly.
+        if data_mean is not None and common_params is not None:
             # Covariates are centered, thus we uncenter the intercept.
             rv = dist(term.label + "_centered", **kwargs, dims=dims)
             data_mean = data_mean.reshape((-1,))
