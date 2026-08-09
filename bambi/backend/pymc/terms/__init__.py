b31ac09656b38b150733517b4daa0bc8153c261f
Tomas Capretto <tomicapretto@gmail.com>
2026-08-09 23:18:40 +0300

    Fix HSGP contributions in conditional predictors

--- bambi/backend/pymc/terms/__init__.py
+++ bambi/backend/pymc/terms/__init__.py
@@ -3,6 +3,7 @@ from bambi.backend.pymc.terms.group_specific import (
     build_group_specific_term_dot,
     build_group_specific_term_idx,
 )
+from bambi.backend.pymc.terms.hsgp import build_hsgp_term
 from bambi.backend.pymc.terms.intercept import build_intercept_term
 from bambi.backend.pymc.terms.potentials import build_potentials
 from bambi.backend.pymc.terms.response import build_response_term
@@ -11,6 +12,7 @@ __all__ = [
     "build_common_term",
     "build_group_specific_term_dot",
     "build_group_specific_term_idx",
+    "build_hsgp_term",
     "build_intercept_term",
     "build_potentials",
     "build_response_term",
