c368bfb0066da8e6c61bcc5f8e7b58bd9131654b
Tomas Capretto <tomicapretto@gmail.com>
2026-08-10 13:19:40 +0300

    temp: bring back predictions in new groups

--- bambi/backend/pymc/parameters/__init__.py
+++ bambi/backend/pymc/parameters/__init__.py
@@ -1,4 +1,5 @@
 from bambi.backend.pymc.parameters.conditional import (
+    add_new_group_specific_contributions,
     build_conditional_parameter,
     build_omitted_group_offsets,
     remove_group_specific_contributions,
@@ -7,6 +8,7 @@ from bambi.backend.pymc.parameters.marginal import build_marginal_parameter
 
 __all__ = [
     "build_conditional_parameter",
+    "add_new_group_specific_contributions",
     "build_omitted_group_offsets",
     "remove_group_specific_contributions",
     "build_marginal_parameter",
