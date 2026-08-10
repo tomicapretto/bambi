eefdcb8971c52474a0cb8de5a2cded06bfe793ab
Tomas Capretto <tomicapretto@gmail.com>
2026-08-10 12:48:39 +0300

    Restore group-specific excelusion in predictions

--- bambi/backend/pymc/parameters/__init__.py
+++ bambi/backend/pymc/parameters/__init__.py
@@ -1,11 +1,13 @@
 from bambi.backend.pymc.parameters.conditional import (
     build_conditional_parameter,
     build_omitted_group_offsets,
+    remove_group_specific_contributions,
 )
 from bambi.backend.pymc.parameters.marginal import build_marginal_parameter
 
 __all__ = [
     "build_conditional_parameter",
     "build_omitted_group_offsets",
+    "remove_group_specific_contributions",
     "build_marginal_parameter",
 ]
