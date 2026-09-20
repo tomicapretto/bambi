e87d9bcba267ad08ebb8c3499ded58b6f334ea00
Tomas Capretto <tomicapretto@gmail.com>
2026-09-20 14:22:32 -0300

    Extract 'scale_priors' from the prior module to its own scaling module

--- bambi/priors/__init__.py
+++ bambi/priors/__init__.py
@@ -1,9 +1,7 @@
 """Classes to represent prior distributions and methods to set automatic priors"""
 
 from bambi.priors.prior import Prior
-from bambi.priors.scaler import scale_priors
 
 __all__ = [
     "Prior",
-    "scale_priors",
 ]
